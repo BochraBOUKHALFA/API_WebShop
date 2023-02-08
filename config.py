0c7ae49f8ef3ed4afeec8e50ca52ba5a93383dba
small project <boukhalfabouchra101@gmail.com>
2023-02-08 11:39:25 +0100

    Untracked files in .gitingore

--- config.py
+++
@@ -1,6 +0,0 @@
-KEY ='BOCHRA'
-END_POINT_ERP = "https://615f5fb4f7254d0017068109.mockapi.io/api/v1/products"
-END_POINT_CRM = "https://615f5fb4f7254d0017068109.mockapi.io/api/v1/customers"
-END_POINT_CRM2 = "https://615f5fb4f7254d0017068109.mockapi.io/api/v1/customers/{customer_id}/orders/{order_id}/products"
-
-
