f9c6e0e117a94b8b52efe37f56990dc0b779a3a5
small project <boukhalfabouchra101@gmail.com>
2023-02-09 11:41:01 +0100

    changed the key added a setup.cfg and setup.py for packaging the project

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 KEY ='ghp_NWg51oDOp4g6dMz0SeKHEVE2EgR5X80hBFIO'
 END_POINT_ERP = "https://615f5fb4f7254d0017068109.mockapi.io/api/v1/products"
 END_POINT_CRM = "https://615f5fb4f7254d0017068109.mockapi.io/api/v1/customers"
-END_POINT_CRM2 = "https://615f5fb4f7254d0017068109.mockapi.io/api/v1/customers/{customer_id}/orders/{order_id}/products"
-
 
